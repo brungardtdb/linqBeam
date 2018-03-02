c076ffc8c8646c3e5722f3a61a669492a8611bd7
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2018-03-01 19:57:41 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace xmlDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf=8", "yes"),
+                new XComment("Creating an XML Tree using LINQ to XML"),
+                new XElement("SteelMembers",
+
+                from steelmembers in SteelMember.GetSteelMembers()
+                select new XElement("SteelMember", new XAttribute("Id", steelmembers.Id),
+                new XElement("Profile", steelmembers.Profile),
+                new XElement("Primer", steelmembers.Primer),
+                new XElement("Length", steelmembers.Length))));
+                
+
+            xmlDocument.Save(@"C:\Users\brung\Desktop\Programming\Software Development\C#\C#\Linq XML\linqBeam\linqBeam\Steel.xml");
+        }
+    }
+}
---
+++ SteelMembers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xmlDemo
+{
+    class SteelMember
+    {
+        public int Id { get; set; }
+        public string Profile { get; set; }
+        public string Primer { get; set; }
+        public double Length { get; set; }
+
+
+        public static SteelMember[] GetSteelMembers()
+        {
+            SteelMember[] steelmembers = new SteelMember[4];
+
+            steelmembers[0] = new SteelMember { Id = 101, Profile = "W12X10", Primer = "Red", Length = 120.68 };
+            steelmembers[1] = new SteelMember { Id = 102, Profile = "L5X5X3/8", Primer = "Grey", Length = 206.75 };
+            steelmembers[2] = new SteelMember { Id = 103, Profile = "C10X15.3", Primer = "Red", Length = 72.88};
+            steelmembers[3] = new SteelMember { Id = 104, Profile = "W12X18", Primer = "Black", Length = 468.38 };
+
+            return steelmembers;
+        }
+    }
+}
